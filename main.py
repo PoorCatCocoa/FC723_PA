a2bba16b2b29c39cfc9dde941a973a3119372df7
PoorCatCocoa <104686890+PoorCatCocoa@users.noreply.github.com>
2025-01-26 20:19:25 +0000

    Euclidean Algorithm with User Input

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ While b != 0{		Use the Euclidean Algorithm to find the GCD(a) when b = 0
 }
 return a;
 """
+from PyQt5.sip import voidptr
+
 
 class EuclideanAlgorithm:		#Trans the pseudocode to python code with OOP
-	def __init__(self, a, b):
+	def __init__(self, a: int, b: int):
 		self.a = a
 		self.b = b
 
@@ -20,7 +22,12 @@ class EuclideanAlgorithm:		#Trans the pseudocode to python code with OOP
 			r = self.a % self.b
 			self.a = self.b
 			self.b = r
-		return self.a
+		return print("The GCD is:", self.a)
 
-a = EuclideanAlgorithm(12, 15)
-print(a.gcd())
+print("Welcome to the Euclidean Algorithm!\n"
+	  "You can enter two numbers to find the greatest common divisor(GCD) of them.\n")
+
+print(EuclideanAlgorithm(int(input("Please enter the first number\n")),
+						 int(input("Please enter the second number\n"))).gcd())
+
+
